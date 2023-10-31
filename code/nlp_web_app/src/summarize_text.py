bb37aca90ecc2de3d30531e9e6670c70e00349d3
Sina Nazem <92341812+sinanazem@users.noreply.github.com>
2023-10-31 09:22:22 +0330

    Update summarize_text.py

--- code/nlp_web_app/src/summarize_text.py
+++ code/nlp_web_app/src/summarize_text.py
@@ -3,5 +3,5 @@ from transformers import pipeline
 
 def summarize_BART(text):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn", truncation=True)
-    summarize_list_of_dict = summarizer(text, max_length=1000, min_length=30, do_sample=False)
+    summarize_list_of_dict = summarizer(text, max_length=1000, min_length=45, do_sample=False)
     return summarize_list_of_dict[0]['summary_text']
